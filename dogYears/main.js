5ff328a0cdb001d6d07f1300248ab321f04b63fb
Phoenix Smith <phoenix.smith.bcbsnc@gmail.com>
2020-09-15 19:39:25 -0400

    Added variables for age calculations.

---
+++ dogYears/main.js
@@ -0,0 +1,17 @@
+// My age as a human right now
+const myAge = 27;
+
+// Early years of the dog's life span
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5; // can also be written as earlyYears *= 10.5;
+
+//Years after the "early years" stage of the dog's life
+let laterYears = myAge - 2;
+
+// After the early years a dog's life span is quadrupled
+laterYears *= 4;
+
+// checking my work
+console.log(earlyYears);
+console.log(laterYears);
