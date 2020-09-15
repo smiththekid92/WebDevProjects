2c8be7656f0d352f8fd8ced33665dbe56a1d082d
Phoenix Smith <phoenix.smith.bcbsnc@gmail.com>
2020-09-15 19:08:16 -0400

    Initial commit. Added files created variables w/ comments

---
+++ kelvinWeather/main.js
@@ -0,0 +1,8 @@
+// Today's forecast in Kelvin
+const kelvin = 293;
+
+// Degrees in Celsius (273 less than kelvin temp)
+ const celsius = kelvin - 273;
+
+ // Fahrenheit to Celsius conversion
+ let fahrenheit = celsius * (9/5) + 32;
