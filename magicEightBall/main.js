db2a83135d9731d213c963ab830c9f9eba1b53de
Phoenix Smith <phoenix.smith.bcbsnc@gmail.com>
2020-09-18 18:41:57 -0400

    Added switch statement for eight ball response

--- magicEightBall/main.js
+++ magicEightBall/main.js
@@ -1,10 +1,46 @@
-let userName = "";
+let userName = '';
 
 userName ? console.log(`Hello ${userName}!`) :
   console.log('Hello!');
 
-const userQuestion = 'Should I take a trip this winter?';
+const userQuestion = 'Should I get a dog?';
 
 console.log(`${userQuestion}`);
 
 const randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (eightBall) {
+  case 0:
+  eightBall = 'Yes!';
+  break;
+
+  case 1:
+  eightBall = 'Definitely not!';
+  break;
+
+  case 2:
+  eightBall = 'I\'m not so sure about that...';
+  break;
+
+  case 3:
+  eightBall = 'Nah.';
+  break;
+
+  case 4:
+  eightBall = 'Cannot predict now';
+  break;
+
+  case 5:
+  eightBall = 'The fact that you\'re asking is a sign ;)';
+  break;
+
+  case 6:
+  eightBall = 'You might want to think that through some more.';
+  break;
+
+  case 7:
+  eightBall = 'It is what it is.';
+  break;
+}
