d3b8e2f52cd47833040a67e022763dd9e6e4239d
Phoenix Smith <phoenix.smith.bcbsnc@gmail.com>
2020-09-18 18:33:51 -0400

    Added question and expression for number generator

--- magicEightBall/main.js
+++ magicEightBall/main.js
@@ -2,3 +2,9 @@ let userName = "";
 
 userName ? console.log(`Hello ${userName}!`) :
   console.log('Hello!');
+
+const userQuestion = 'Should I take a trip this winter?';
+
+console.log(`${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random() * 8);
