3b7c419bac38ff2b3fc863db9720f0f203a40b56
Phoenix Smith <phoenix.smith.bcbsnc@gmail.com>
2020-09-24 21:44:51 -0400

    Added computer imput logic

--- rockPaperScissors/main.js
+++ rockPaperScissors/main.js
@@ -1,4 +1,4 @@
-//User Choice function & logic
+//User choice function & logic
 
 const getUserChoice = userInput => {
   userInput = userInput.toLowerCase();
@@ -9,3 +9,18 @@ const getUserChoice = userInput => {
     console.log('Error: Please choose a valid input (rock, paper, or scisors');
   }
 }
+
+// Computer choice function & logic
+
+function getComputerChoice(){
+  const ranNumber = Math.floor(Math.random() * 3);
+  switch(ranNumber) {
+    case 0: 
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2: 
+      return 'scissors';
+  }
+}
+
