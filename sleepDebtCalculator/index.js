6f4ff2cdc964b6ff14f9430d3ed46a0d5d82fdbb
Phoenix Smith <phoenix.smith.bcbsnc@gmail.com>
2020-09-29 21:11:25 -0400

    Added functions for actual sleep hours and ideal sleep hours. Added Friday & Saturday

--- sleepDebtCalculator/index.js
+++ sleepDebtCalculator/index.js
@@ -15,6 +15,11 @@ const getSleepHours = day => {
     case 'Thursday' :
       return 6;
       break;
+    case 'Friday' :
+      return 8;
+      break;
+    case 'Saturday' :
+      return 8;
     default : 
       console.log('No data available');
   }
@@ -30,5 +35,11 @@ console.log(getSleepHours('Friday')); //Output: No data available
 
 console.log(getSleepHours('Wednesday')); //Output: 7 */
 
+const getActualSleepHours = () => 
+  getActualSleepHours('Sunday') + getActualSleepHours('Monday') + getActualSleepHours('Tuesday') + getActualSleepHours('Wednesday') + getActualSleepHours('Thursday') + getActualSleepHours('Friday') + getActualSleepHours('Saturday')
 
 
+const getIdealSleepHours = () => {
+  let idealHours = 8;
+  return idealHours * 7;
+}
